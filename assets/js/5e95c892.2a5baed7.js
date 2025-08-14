bd302c523df6c92c85dbb44440b22434c07f9e70
AloyASen <AloyASen@users.noreply.github.com>
2025-08-14 08:06:23 +0000

    deploy: 37545a9b7d29983b03b3089254a17127fb2c634b

---
+++ assets/js/5e95c892.2a5baed7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkscaffold=self.webpackChunkscaffold||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>n});r(6540);var a=r(4164),c=r(7559),u=r(5500),f=r(2831),l=r(1656),d=r(4848);function n(e){return(0,d.jsx)(u.e3,{className:(0,a.A)(c.G.wrapper.docsPages),children:(0,d.jsx)(l.A,{children:(0,f.v)(e.route.routes)})})}}}]);
